dba7e0ddb37f1a0180b08e12d384ba0b2bc54489
hackshs <60843044+hackshs@users.noreply.github.com>
2020-02-11 18:29:08 +0200

    Create Mohab.py

---
+++ Mohab.py
@@ -0,0 +1,13 @@
+import os
+
+os.system ( hello in mohab make this )
+
+print( apt update )
+
+print( apt upgrade )
+
+print( pkg install python -y )
+
+print( pkg install python2 -y )
+
+print( pkg install python3 -y )
